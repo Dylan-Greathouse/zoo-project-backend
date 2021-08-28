1dc45dfc86d0bb6ab6e30b691f53d33f54fe4466
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-28 11:10:28 -0700

    added keys and working on animal data

---
+++ data/rainforest.js
@@ -0,0 +1,63 @@
+module.exports = [{
+  name: 'Mountain Gorilla',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+
+{
+  name: 'Okapi',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+
+{
+  name: 'Sloth',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+
+{
+  name: 'Capybara',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+
+{
+  name: 'Polar Bear',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+
+{
+  name: 'Howler Monkey',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+
+{
+  name: 'Howler Monkey',
+  species_name: 'Ursus Maritimus',
+  habitat: 'Rainforests',
+  image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Polar_Bear_-_Alaska_%28cropped%29.jpg/220px-Polar_Bear_-_Alaska_%28cropped%29.jpg',
+  icon_url: '',
+  description: 'The polar bear is a marine mammal because it spends many months of the year at sea. However, it is the only living marine mammal with powerful, large limbs and feet that allow them to cover kilometers on foot and run on land. Its preferred habitat is the annual sea ice covering the waters over the continental shelf and the Arctic inter-island archipelagos.',
+  diet: 'carnivore' },
+];
